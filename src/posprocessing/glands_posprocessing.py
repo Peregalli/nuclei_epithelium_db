1a2f38e53da74dd7ead9dbe440f75833f873b08c
Agustina <9101128@ulta.com>
2023-10-24 18:25:37 -0300

    Glands posprocessing compleated. It saves in a Pandas DataFrame all the information and plots some example images and relative area histograms.
    TODO : Add in extract_all_patches.py at report.json  which glands model is using. Also add a prefix with that information to dst_folder

--- src/posprocessing/glands_posprocessing.py
+++ src/posprocessing/glands_posprocessing.py
@@ -4,53 +4,77 @@ import os
 import json
 import pandas as pd
 from tqdm import tqdm 
+from utils.plot_utils import glands_visualization_relative_area, plot_glands_histogram, plot_glands_histogram_comparison
 
-class Posprocessing():
-    def __init__(self,nuclei_channel,epithelium_channel):
-        self.nuclei_channel = nuclei_channel 
-        self.epithelium_channel = epithelium_channel
-        self.kernel_size = 3
-        self.kernel = np.ones((self.kernel_size, self.kernel_size), np.uint8)
-        self.glands_proportion = 0.0035
+class GlandsPosprocessing():
+    def __init__(self,path_to_folder : str):
+        self.path_to_folder = path_to_folder
+        self.params = self.__get_params()
+        self.min_relative_area = 0.005
+        self.max_relative_area = None  
+        self.path_to_masks = os.path.join(path_to_folder,'new_masks')
     
-    def apply(self, mask : np.ndarray, mask_fn : str, )-> np.ndarray:
+    def extract_features(self, save_mask_instances : bool = True):
 
+        if os.path.exists(os.path.join(self.path_to_folder,'glands_report.csv')):
+            self.glands_data = pd.read_csv(os.path.join(self.path_to_folder,'glands_report.csv'), index_col = 'gland_id')
+            return
 
-        return mask
+        # Initialize
+        df = pd.DataFrame(columns=['gland_id', 'patch_loc', 'patch_gland_ind', 'relative_area'])
+        df = df.set_index('gland_id')
+        gland_id = 0
+        channel_glands = self.params['channel_epithelium']
+
+        # Open mask
+        for mask_fn in tqdm(os.listdir(self.path_to_masks)):
+
+            mask_ori = cv.imread(os.path.join(self.path_to_masks,mask_fn))
+            mask = mask_ori[:,:,channel_glands]
+            patch_loc = (mask_fn.split('.')[0]).split('_')[-2:]
+            # Separate in instances
+            num_labels, glands_instances, stats, centroids = cv.connectedComponentsWithStats(mask)
+            for i in range(1,num_labels):
+                relative_area = np.sum(glands_instances == i)/(self.params['patch_size']*self.params['patch_size'])
+                if relative_area > self.min_relative_area:
+                    df.loc[gland_id] = {'patch_loc': patch_loc[0]+'_'+patch_loc[1],'patch_gland_ind' : i, 'relative_area' : relative_area}
+                    gland_id += 1 
+
+            if save_mask_instances :
+                mask_ori[:,:,channel_glands] = glands_instances
+                cv.imwrite(os.path.join(self.path_to_masks,mask_fn), mask)
+
+        df.to_csv(os.path.join(self.path_to_folder,'glands_report.csv'))        
+        self.glands_data = df
+    
+    def plot_results(self):
+        glands_visualization_relative_area(self.glands_data, self.params, self.path_to_folder)
+        plot_glands_histogram(self.glands_data, self.path_to_folder, self.min_relative_area)
+
+    def __get_params(self):
+        assert os.path.exists(os.path.join(self.path_to_folder,'report.json')), 'report.json not found. Run extract_all_patches.py first'
+        with open(os.path.join(self.path_to_folder,'report.json'), 'r') as config_file:
+            params = json.load(config_file)
+        return params
+    
+    def compare_with(self, glands_posprocessing_to_compare):
+        wsi_name = os.path.basename(self.path_to_folder)
+        wsi_name_to_compare = os.path.basename(glands_posprocessing_to_compare.path_to_folder)
+        plot_glands_histogram_comparison(self.glands_data, glands_posprocessing_to_compare.glands_data, wsi_name,wsi_name_to_compare, self.min_relative_area)
 
 def main():
-    PATH_TO_FOLDER = '/home/agustina/Documents/FING/proyecto/nuclei_epithelium_db/44-D5_glands'
-    PATH_TO_MASKS = os.path.join(PATH_TO_FOLDER,'new_masks')
-    PATH_TO_PATCHES = os.path.join(PATH_TO_FOLDER,'patches')
-    
-    with open(os.path.join(PATH_TO_FOLDER,'report.json'), 'r') as config_file:
-        params = json.load(config_file)
-    
-    channel_glands = params['channel_epithelium']
+    PATH_TO_FOLDER_1 = '/home/agustina/Documents/FING/proyecto/nuclei_epithelium_db/44-D5_glands'
+    PATH_TO_FOLDER_2 = '/home/agustina/Documents/FING/proyecto/nuclei_epithelium_db/44-D5_epithelium'   
 
-    # Initialize dataframe
-    df = pd.DataFrame(columns=['gland_id', 'patch_loc', 'patch_gland_ind', 'relative_area'])
-    df = df.set_index('gland_id')
-    gland_id = 0
-    TRESH_AREA = 0.005
+    glands_posprocessing_1 = GlandsPosprocessing(PATH_TO_FOLDER_1)
+    glands_posprocessing_1.extract_features()
+    glands_posprocessing_1.plot_results()
 
-    # Open mask
-    for mask_fn in tqdm(os.listdir(PATH_TO_MASKS)):
+    glands_posprocessing_2 = GlandsPosprocessing(PATH_TO_FOLDER_2)
+    glands_posprocessing_2.extract_features()
+    glands_posprocessing_2.plot_results()
 
-        mask_ori = cv.imread(os.path.join(PATH_TO_MASKS,mask_fn))
-        mask = mask_ori[:,:,channel_glands]
-        patch_loc = (mask_fn.split('.')[0]).split('_')[-2:]
-        # Separate in instances
-        num_labels, glands_instances, stats, centroids = cv.connectedComponentsWithStats(mask)
-        for i in range(1,num_labels):
-            relative_area = np.sum(glands_instances == i)/(params['patch_size']*params['patch_size'])
-            if relative_area > TRESH_AREA:
-                df.loc[gland_id] = {'patch_loc': patch_loc[0]+'_'+patch_loc[1],'patch_gland_ind' : i, 'relative_area' : relative_area}
-                gland_id += 1 
-        
-        mask_ori[:,:,channel_glands] = glands_instances
-        cv.imwrite(os.path.join(PATH_TO_MASKS,mask_fn), mask)
-    
-    df.to_csv(os.path.join(PATH_TO_FOLDER,'glands_report.csv'))
+    glands_posprocessing_2.compare_with(glands_posprocessing_1)
+
 if __name__ == "__main__":
     main()
--- src/utils/plot_utils.py
+++ src/utils/plot_utils.py
@@ -4,6 +4,8 @@ import os
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from matplotlib.colors import Normalize
 from matplotlib.cm import ScalarMappable
+import cv2 as cv
+import pandas as pd
 
 def density_color_map_plot(density_map : np.ndarray, gland_channel : int, nuclei_channel : int, title : str, dst_folder : str):
 
@@ -35,7 +37,102 @@ def density_color_map_plot(density_map : np.ndarray, gland_channel : int, nuclei
     ax.axis('off')
     
     plt.savefig(os.path.join(dst_folder,'density_map.png'))
+    return
 
+
+def glands_visualization_relative_area(df : pd.DataFrame,config : dict, root_to_folder : str):
+    
+    WSI_NAME, model = os.path.basename(root_to_folder).split('_')
+    PATH_TO_FOLDER = root_to_folder
+    
+    if model == 'epithelium':
+        color = (255,0,0)
+    else :
+        color = (0,0,255)
+        
+    range_areas = np.arange(0,1.2,0.2)
+    fig, axs = plt.subplots(1, len(range_areas) -1, figsize = (25, 5))
+    fig.suptitle(f'Relative area per gland instance - {model} model', fontsize = 20)
+
+    for i in range(len(range_areas) -1):
+        min_v = range_areas[i]
+        max_v = range_areas[i+1]
+        patch_loc_counts = df[(min_v < df.relative_area)&(df.relative_area < max_v)]['patch_loc'].value_counts()
+        
+        if len(patch_loc_counts) != 0 :
+            patch_loc = patch_loc_counts.idxmax()
+            
+            mask_fn = os.path.basename(root_to_folder)+'_'+str(config['level'])+'_'+patch_loc+'.png'
+            PATH_TO_MASK = os.path.join(PATH_TO_FOLDER,'new_masks',mask_fn)
+            PATH_TO_IMG = os.path.join(PATH_TO_FOLDER,'patches',mask_fn)
+            df_patch = df[df.patch_loc == patch_loc]
+            
+            gland_mask = cv.imread(PATH_TO_MASK)[:,:,config['channel_epithelium']]
+            img = cv.imread(PATH_TO_IMG)[:,:,::-1]
+            interes_glands = np.zeros((gland_mask.shape))
+            for index in df_patch[(min_v < df_patch.relative_area)&(df_patch.relative_area < max_v)].patch_gland_ind:
+                interes_glands = interes_glands + (gland_mask == index)
+            
+            contours, hierarchy = cv.findContours((255*interes_glands).astype(np.uint8), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+            img = cv.drawContours(img.astype(np.uint8),contours, -1, color, 7)
+        else:
+            img = np.ones((config['patch_size'],config['patch_size']))
+            gland_mask = img.copy()
+        ax = axs[i]
+        ax.imshow(img)
+        ax.imshow(gland_mask> 0,alpha = 0.3,cmap = 'gray')
+        ax.axis('off')
+        ax.set_title(f'Relative area between {round(min_v, 1)} - {round(max_v,1)}', fontsize = 15)
+    
+    plt.savefig(os.path.join(PATH_TO_FOLDER, f'Relative_area_per_gland_instance_{model}.png'))
+    plt.show()
+    return
+               
+def plot_glands_histogram_comparison(df_1 : pd.DataFrame, df_2 : pd.DataFrame, wsi_name_1 : str, wsi_name_2 : str, threshold_area : float = 0.005,
+                                    output_dir : str = './'):
+    
+    df_2 = df_2[df_2.relative_area > threshold_area]
+    df_1 = df_1[df_1.relative_area > threshold_area]
+
+    c_epith = 'red'
+    c_glands = 'blue'
+
+    plt.figure(figsize = (25,10))
+    plt.hist(df_2.relative_area, bins=50, color = c_epith, alpha = 0.6, label = f'{wsi_name_1}')
+    plt.hist(df_1.relative_area, bins=50, color = c_glands, alpha = 0.6, label = f'{wsi_name_2}')
+    plt.yscale('log')
+    plt.xticks(fontsize=15)  # Change the font size as needed
+    plt.yticks(fontsize=15)
+    plt.legend(fontsize=20)
+    plt.title(f'Relative area histogram comparison', fontsize = 25)
+    plt.grid()
+    
+    plt.savefig(os.path.join(output_dir,f'{wsi_name_1}_vs_{wsi_name_2}_plot_glands_histogram_comparison.png'))
+    plt.show()
+    return
+
+def plot_glands_histogram(df : pd.DataFrame, root_to_folder : str, threshold_area : float = 0.005):
+
+    wsi_name, model = os.path.basename(root_to_folder).split('_')
+    if model == 'epithelium':
+        color = 'red'
+    else :
+        color = 'blue'
+
+    df = df[df.relative_area > threshold_area]    
+
+    plt.figure(figsize = (25,10))
+    plt.hist(df.relative_area, bins=50, color = color, alpha = 0.6, label = f'{model} model')
+    plt.yscale('log')
+    plt.xticks(fontsize=15)  # Change the font size as needed
+    plt.yticks(fontsize=15)
+    plt.legend(fontsize=20)
+    plt.title(f'Glands area histograms for {wsi_name}.svs', fontsize = 25)
+    plt.grid()
+    
+    plt.savefig(os.path.join(root_to_folder,f'{wsi_name}_plot_glands_histogram_model.png'))
+    plt.show()
+    return
 
 def main():
     PATH_TO_FOLDER = '/home/agustina/Documents/FING/proyecto/nuclei_epithelium_db/Lady_epithelium'
