6d4a620985166665945b774df5462f188ccb2319
sofia-zimmer <sofia.zimmer@gmail.com>
2023-11-14 17:16:08 -0300

    Updated NucleiPosprocessing class

--- src/posprocessing/nuclei_posprocessing.py
+++ src/posprocessing/nuclei_posprocessing.py
@@ -4,55 +4,47 @@ sys.path.append('src/')
 import cv2
 import os
 import json
+import joblib
 import pandas as pd
+import seaborn as sns
 from tqdm import tqdm
-from scipy.stats import entropy 
-
+from scipy.stats import entropy
+import plotly.graph_objects as go
 import argparse
-from utils.plot_utils import glands_visualization_relative_area, plot_glands_histogram, plot_glands_histogram_comparison, subplot_masks
+from utils.plot_utils import nuclei_high_circularity, plot_results_kmeans
 
-parser = argparse.ArgumentParser(description='Get glands features from masks.')
+parser = argparse.ArgumentParser(description='Get nuclei features from masks.')
 parser.add_argument('-s', '--src_folder', help="path to folder that mask and patches are saved", type=str)
-parser.add_argument('-c', '--compare_folder', help="Another folder to compare with src_folder.", default= None, type=str)
+
 
 class NucleiPosprocessing():
     def __init__(self,path_to_folder : str):
         self.path_to_folder = path_to_folder
         self.params = self.__get_params()
-        self.min_area = 10  
+        self.selected_images = None
+        self.min_area = 10
+        self.min_features_values = np.array([1.09020000e+01, 5.83402388e-02, 9.21593688e+00, 4.75859961e-05, 6.43694598e-03, 2.72346790e+00])
+        self.max_features_values = np.array([2.48667600e+03, 5.97835209e-01, 1.94816522e+02, 6.84880965e-02, 1.93625251e-01, 7.46806437e+00])  
         self.path_to_masks = os.path.join(path_to_folder,'new_masks')
         self.path_to_images = os.path.join(path_to_folder,'patches')
 
-    def __get_texture_R(intensity_levels):
+    def __get_params(self):
+        assert os.path.exists(os.path.join(self.path_to_folder,'report.json')), 'report.json not found. Run extract_all_patches.py first'
+        with open(os.path.join(self.path_to_folder,'report.json'), 'r') as config_file:
+            params = json.load(config_file)
+        return params
+
+    def __get_texture_R(self, intensity_levels):
         norm_var = intensity_levels.var()/(255**2)
         return 1-1/(1+norm_var)
 
-    def __get_texture_U(p_intensity):
+    def __get_texture_U(self, p_intensity):
         return np.sum(p_intensity[0]**2)
 
-    def __get_texture_E(p_intensity):
+    def __get_texture_E(self, p_intensity):
         return entropy(p_intensity[0], base=2)
-    
-    def nuclei_feature_extraction(image: np.ndarray, mask: np.ndarray)-> dict:
 
-        '''
-        cv2 documentation about functions use in this script:
-
-        cv2.connectedComponentsWithStats outputs:
-            num_labels: The total number of labels [0, ..., N-1] where 0 represents the background label.
-            nuceli_instances: A grayscale image where each pixel is the label of the object at that location.
-            stats: A 2D array where each row is a 5-dim vector that contains the following information, in order:
-                CC_STAT_LEFT: The leftmost (x) coordinate which is the inclusive start of the bounding box in the horizontal direction.
-                CC_STAT_TOP: The topmost (y) coordinate which is the inclusive start of the bounding box in the vertical direction.
-                CC_STAT_WIDTH: The horizontal size of the bounding box.
-                CC_STAT_HEIGHT: The vertical size of the bounding box.
-                CC_STAT_AREA: The total area (in pixels) of the connected component.
-            centroids: A 2D array where each row indicates the (x, y) coordinate of a centroid. The row corresponds to the label number. 
-
-        cv2.findContours parameters:
-            mode -> cv2.RETR_EXTERNAL: retrieves only the extreme outer contours.
-            method -> cv2.CHAIN_APPROX_SIMPLE: compresses horizontal, vertical, and diagonal segments and leaves only their end points.
-        '''
+    def __get_nuclei_features(self, image: np.ndarray, mask: np.ndarray)-> dict:
 
         # 1. Separate all nuclei instances and calculate statistics.
         num_labels, nuclei_instances, stats, centroids = cv2.connectedComponentsWithStats(mask)
@@ -88,119 +80,107 @@ class NucleiPosprocessing():
 
             # 3. Calculate the probability density function of the intensity levels in the nucleus in order to calculate different metrics regarding nucleus texture
             p_intensity = np.histogram(intensity_levels, bins=np.arange(intensity_levels.min(), intensity_levels.max()+2), density=True)
-            texture_R.append(R(intensity_levels))
-            texture_U.append(U(p_intensity))
-            texture_E.append(E(p_intensity))
+            texture_R.append(self.__get_texture_R(intensity_levels))
+            texture_U.append(self.__get_texture_U(p_intensity))
+            texture_E.append(self.__get_texture_E(p_intensity))
 
             # 4. Save data of all nuclei in a dictionary.
             
         return {'contour':contours, 'perimeter': perimeters, 'area':areas, 'circularity':circularity, 
                 'intensity':intensity, 'texture_R': texture_R, 'texture_U': texture_U, 'texture_E': texture_E}
-    
-    
+        
     def extract_features(self):
 
-        if os.path.exists(os.path.join(self.path_to_folder,'glands_report.csv')):
-            self.glands_data = pd.read_csv(os.path.join(self.path_to_folder,'glands_report.csv'), index_col = 'gland_id')
-            return
+        images_names= os.listdir(self.path_to_images)
 
-        # Initialize
-        df = pd.DataFrame(columns=['gland_id', 'patch_loc', 'patch_gland_ind', 'relative_area'])
-        df = df.set_index('gland_id')
-        gland_id = 0
-        channel_glands = self.params['channel_glands']
+        # Create df to save nuclei data
+        nuclei_df = pd.DataFrame(columns=['image_name', 'contour', 'perimeter', 'area', 'circularity', 'intensity', \
+                                      'texture_R', 'texture_U', 'texture_E'])
 
-        # Open mask
-        for mask_fn in tqdm(os.listdir(self.path_to_masks)):
+        # Load images and masks and extract nuclei features
+        for image_name in tqdm(images_names):
 
-            mask_ori = cv.imread(os.path.join(self.path_to_masks,mask_fn))
-            mask = mask_ori[:,:,channel_glands]
-            patch_loc = (mask_fn.split('.')[0]).split('_')[-2:]
-            # Separate in instances
-            num_labels, glands_instances, stats, centroids = cv.connectedComponentsWithStats(mask)
-            for i in range(1,num_labels):
-                relative_area = np.sum(glands_instances == i)/(self.params['patch_size']*self.params['patch_size'])
-                if relative_area > self.min_relative_area:
-                    df.loc[gland_id] = {'patch_loc': patch_loc[0]+'_'+patch_loc[1],'patch_gland_ind' : i, 'relative_area' : relative_area}
-                    gland_id += 1 
+             # Load image and mask
+            image = cv2.imread(os.path.join(self.path_to_images, image_name))
+            mask = cv2.imread(os.path.join(self.path_to_masks, image_name))
+            mask = mask[:,:,self.params['channel_nuclei']]
+            # Set a threshold in the mask to binarize it
+            mask[mask>0]=255
+            # Extract nuclei features and save them in the df
+            nuclei_data_dict = self.__get_nuclei_features(image, mask)
+            nuclei_data_dict['image_name'] = image_name
+            tmp_df = pd.DataFrame(nuclei_data_dict)
+            nuclei_df = pd.concat([nuclei_df, tmp_df], ignore_index=True)
+        
+        # Filter instances with area below min_area
+        nuclei_df = nuclei_df[nuclei_df.area>self.min_area]
+            
+        self.nuclei_df = nuclei_df
 
-            if save_mask_instances :
-                mask_ori[:,:,channel_glands] = glands_instances
-                cv.imwrite(os.path.join(self.path_to_masks,mask_fn), mask)
+    def normalize_features(self):
+        '''
+        Normalize each feature using minimum and maximum values extracted from training data
+        '''
+        X_norm = self.nuclei_df[['area', 'circularity', 'intensity', 'texture_R', 'texture_U', 'texture_E']]
+        self.nuclei_df_norm = (X_norm-self.min_features_values)/(self.max_features_values-self.min_features_values)
 
-        df.to_csv(os.path.join(self.path_to_folder,'glands_report.csv'))        
-        self.glands_data = df
+    def plot_normalize_features_histogram(self):
+        features = self.nuclei_df_norm.columns
+
+        for i, feature in enumerate (features):
+            fig = go.Figure()
+            
+            fig.add_trace(go.Histogram(x=self.nuclei_df_norm.iloc[:,i].values, histnorm='probability'))
+            
+            fig.update_layout(
+                title_text='Histogram of ' + feature, # title of plot
+                xaxis_title_text=feature, # xaxis label
+                yaxis_title_text='Probability', # yaxis label
+            )
+            fig.show()
+
+    def plot_nuclei_with_high_circularity(self, percentile: int):
+
+        # Select 3 images that contain nuclei with circularity greater than the percentile choosen
+        candidates_name = self.nuclei_df[self.nuclei_df['circularity']>np.percentile(self.nuclei_df['circularity'],percentile)]['image_name']
+        selected_images = np.random.choice(np.unique(candidates_name), 3, replace=False)
+
+        # Load images
+        images = [cv2.imread(os.path.join(self.path_to_images, image_name)) for image_name in selected_images]
+
+        # Mark in the images the nuclei with high circularity
+        nuclei_high_circularity(images, percentile, self.nuclei_df, selected_images)
+
+        self.selected_images = selected_images
     
-    def plot_results(self, hist_color : str = None):
-        
-        if not self.micro_meters:
-            plot_glands_histogram(self.glands_data, self.path_to_folder, self.min_relative_area,color = hist_color)
-            glands_visualization_relative_area(self.glands_data, self.params, self.path_to_folder)
-        else:   
-            pixel_scale = np.float16(self.params['pixel_scale'])/1000
-            patch_size = self.params['patch_size']
-            glands_data_um2 = self.glands_data.copy()
-            glands_data_um2.relative_area = glands_data_um2.relative_area*((pixel_scale*patch_size)**2)
-
-            glands_visualization_relative_area(glands_data_um2, self.params, self.path_to_folder, max_area=(pixel_scale*patch_size)**2)
-            plot_glands_histogram(glands_data_um2, self.path_to_folder, threshold_area = self.min_relative_area*((pixel_scale*patch_size)**2),color = hist_color, max_value = (pixel_scale*patch_size)**2)
-
-    def __get_params(self):
-        assert os.path.exists(os.path.join(self.path_to_folder,'report.json')), 'report.json not found. Run extract_all_patches.py first'
-        with open(os.path.join(self.path_to_folder,'report.json'), 'r') as config_file:
-            params = json.load(config_file)
-        return params
+    def nuclei_features_pair_plot(self):
+        '''
+        Plot pair plot of the nuclei features
+        '''
+        sns.pairplot(self.nuclei_df_norm, corner=True)
     
-    def compare_with(self, glands_posprocessing_to_compare):
-        wsi_name = os.path.basename(self.path_to_folder)
-        wsi_name_to_compare = os.path.basename(glands_posprocessing_to_compare.path_to_folder)
+    def plot_kmeans_inference(self, kmeans_path: str = 'models/kmeans_5.joblib'):
 
-        if not self.micro_meters:
-            plot_glands_histogram_comparison(self.glands_data, glands_posprocessing_to_compare.glands_data, wsi_name,wsi_name_to_compare, self.min_relative_area)
-        
-        else :
-            # in micrometers change to milimeters
-            pixel_scale = np.float16(self.params['pixel_scale'])/1000
-            patch_size = self.params['patch_size']
-            glands_data_um2 = self.glands_data.copy()
-            glands_data_um2.relative_area = glands_data_um2.relative_area*((pixel_scale*patch_size)**2)
-            glands_data_to_compare_um2 = glands_posprocessing_to_compare.glands_data.copy()
-            glands_data_to_compare_um2.relative_area = glands_data_to_compare_um2.relative_area*((pixel_scale*patch_size)**2)
-            plot_glands_histogram_comparison(glands_data_um2, glands_data_to_compare_um2, wsi_name,wsi_name_to_compare, self.min_relative_area*((pixel_scale*patch_size)**2),
-                                             max_value = (pixel_scale*patch_size)**2)
+        model = joblib.load(kmeans_path)
+        prediction = model.predict(self.nuclei_df_norm.values)
 
-    def get_glands_density_greatter_than(self, relative_area : float):
-        return self.glands_data[self.glands_data['relative_area'] > relative_area]
-    
-    def show_glands_density_greatter_than(self, relative_area : float):
-        glands_density = self.get_glands_density_greatter_than(relative_area)
-        if len(glands_density) == 0:
-            print(f'Glands density greatter than {relative_area} : 0')
-            return
-        
-        elif len(glands_density) > 5:
-            worst_cases = glands_density.sort_values(by=['relative_area'], ascending=False).head(5)
+        if self.selected_images is None:
+            self.selected_images = np.random.choice(np.unique(self.nuclei_df['image_name']), 3, replace=False)
 
-        else :
-            worst_cases = glands_density.sort_values(by=['relative_area'], ascending=False)
-
-        subplot_masks(worst_cases, self.params,self.params['output_dir'] , title = f'Glands density greatter than {relative_area}')
-        
+        # Load images
+        images = [cv2.imread(os.path.join(self.path_to_images, image_name)) for image_name in self.selected_images]
+       
+        plot_results_kmeans(images, self.nuclei_df, self.selected_images, prediction)
 
 
 if __name__ == "__main__":
 
     args = parser.parse_args() 
 
-    glands_posprocessing_1 = GlandsPosprocessing(args.src_folder, micro_meters = True)
-    glands_posprocessing_1.extract_features()
-    glands_posprocessing_1.plot_results()
-
-    glands_posprocessing_1.show_glands_density_greatter_than(0.8)
-
-    if args.compare_folder is not None:
-        glands_posprocessing_2 = GlandsPosprocessing(args.compare_folder, micro_meters = True)
-        glands_posprocessing_2.extract_features()
-        glands_posprocessing_2.plot_results()
-
-        glands_posprocessing_1.compare_with(glands_posprocessing_2)  
+    nuclei_posprocessing = NucleiPosprocessing(args.src_folder)
+    nuclei_posprocessing.extract_features()
+    nuclei_posprocessing.normalize_features()
+    nuclei_posprocessing.plot_normalize_features_histogram()
+    nuclei_posprocessing.plot_nuclei_with_high_circularity(percentile=90)
+    nuclei_posprocessing.nuclei_features_pair_plot()
+    nuclei_posprocessing.plot_kmeans_inference() 
