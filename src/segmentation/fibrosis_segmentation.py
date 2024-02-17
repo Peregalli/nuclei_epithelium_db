87d59618201ef238988a0dc84232eb3948222a0b
nicolas aguilera <nicolas@abya.tech>
2024-02-17 15:10:25 -0300

    implement fibrosis pipeline in FAST

--- src/segmentation/fibrosis_segmentation.py
+++ src/segmentation/fibrosis_segmentation.py
@@ -1,71 +1,102 @@
 import fast
-import numpy as np
-import os
+import time
 import argparse
+import os
 
-parser = argparse.ArgumentParser(description='Fibrosis inference with fibrosis.onnx model.')
-parser.add_argument('-m', '--model', help="model path", type=str, default = 'models/fibrosis.onnx')
-parser.add_argument('-w', '--wsi_path', help="path to wsi", type=str)
-parser.add_argument('-o', '--output_folder', default = 'fibrosis_tiffs', type=str)
+import matplotlib.pyplot as plt
 
+patchSize = 1000
+resize = 0.5
+level = 1
 
-def epithelium_segmentation_wsi(wsi_path : str , model_path : str, output : str):
+def fibrosis_segmentation_wsi(wsi_path : str , model_path : str = 'models/fibrosis.onnx', output_dir : str = None, visualization : bool = False, tissueSegmentationThreshold : int = 85):
+    if output_dir is None:
+        output_dir = os.path.join(os.getcwd(),'fibrosis_tiffs')
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    
+    # Nombre del archivo de salida
+    segmentationFileName = os.path.splitext(os.path.basename(wsi_path))[0]+'.tiff'
 
-    WSI_fn = os.path.splitext(os.path.basename(wsi_path))[0]
+    # Configura el motor de inferencia para usar el orden de canales de PyTorch
+    inferenceEngineList = fast.InferenceEngineManager.getEngineList()
+    if (fast.InferenceEngineManager.isEngineAvailable(inferenceEngineList[0])):
+        print(f"Engine {inferenceEngineList[0]} is available")
+        inferenceEngine = fast.InferenceEngineManager.loadEngine(inferenceEngineList[0])
+        inferenceEngine.setDeviceType(fast.InferenceDeviceType_GPU)
+        inferenceEngine.setImageOrdering(fast.ImageOrdering_ChannelFirst)
+    else:
+        print(f"Engine {inferenceEngineList[0]} is not available")
 
-    print(os.getcwd())
-
-    #Hyperparameters
-    patchSize = 500
-    level = 1
-    overlapPercent = 0.3
-    scaleFactor=1/255
-
+    # Importa una WSI
     importer = fast.WholeSlideImageImporter.create(wsi_path)
 
-    tissueSegmentation = fast.TissueSegmentation.create(threshold = 70).connect(importer)
+    # Segmenta el tejido del fondo
+    wsiRenderer = fast.ImagePyramidRenderer.create().connect(importer)
+    tissueSegmentation = fast.TissueSegmentation.create(threshold=tissueSegmentationThreshold).connect(importer)
 
-    patchGenerator = fast.PatchGenerator.create(patchSize, 
-                                                patchSize, 
-                                                level = level, 
-                                                overlapPercent = overlapPercent)\
-                                                    .connect(0, importer)\
-                                                        .connect(1, tissueSegmentation)
+    # Parte la WSI en parches
+    patchGenerator = fast.PatchGenerator.create(patchSize, patchSize, level=1, overlapPercent=0.1).connect(0,importer).connect(1, tissueSegmentation)
+    # patchGenerator = fast.PatchGenerator.create(args.patchSize, args.patchSize, level=1, overlapPercent=0.1).connect(0,importer)
 
-    # Create neural network object ###
-    nn = fast.NeuralNetwork.create(scaleFactor=scaleFactor,
-                                   modelFilename=model_path,
-                                   ).connect(0, patchGenerator)
+    # Redimensiona los parches
+    imageResizer = fast.ImageResizer.create(int(patchSize*resize), int(patchSize*resize), useInterpolation=True).connect(0, patchGenerator)
 
-    # Create patch stitcher to generate pyramidal tiff output
-    stitcher = fast.PatchStitcher.create().connect(0, nn)
+    # Genera un batch de parches
+    maxBatchSize = 1
+    batchGenerator = fast.ImageToBatchGenerator.create(maxBatchSize=maxBatchSize).connect(0, imageResizer)
 
+    # Crea una red neuronal a partir de un archivo ONNX
+    nn = fast.NeuralNetwork.create(scaleFactor=0.003921568627451,modelFilename=model_path).connect(0, batchGenerator)
 
-    # Create renderers to show both original WSI and segmentation output
-
-    wsiRenderer = fast.ImagePyramidRenderer.create().connect(0, importer)
-    segmentationRenderer = fast.SegmentationRenderer.create(opacity = 0.1,borderOpacity=1, colors={1: fast.Color.Green(), 2:fast.Color.Red()}).connect(stitcher)
-
-    # Create window to display segmentation
-    fast.SimpleWindow2D.create()\
-            .connect(wsiRenderer)\
-                .connect(segmentationRenderer).run()
-
-
-    finished = fast.RunUntilFinished.create()\
-        .connect(stitcher)
-
-    if not os.path.exists(output):
-        os.mkdir(output)
-
-    exporter = fast.TIFFImagePyramidExporter.create(os.path.join(output,WSI_fn+'.tiff'))\
-        .connect(finished)\
-        .run()
+    # Convierte el tensor de inferencia a una segmentacion
+    tensorToSegmentation = fast.TensorToSegmentation.create(hasBackgroundClass=True).connect(0, nn)
     
-    return
+    # Redimensiona los parches inferidos a la resolucion original
+    imageResizer = fast.ImageResizer.create(int(patchSize), int(patchSize), useInterpolation=True).connect(0, tensorToSegmentation)
     
+    # Junta la inferencia de los parches en un solo archivo TIFF
+    stitcher = fast.PatchStitcher.create(patchesAreCropped=False).connect(0, imageResizer)
+
+    finished = fast.RunUntilFinished.create().connect(stitcher)
+    
+    # Guarda el TIFF a disco
+    exporter = fast.TIFFImagePyramidExporter.create(os.path.join(output_dir, segmentationFileName)).connect(finished)
+
+    if visualization:
+        ### Create window and renderers to display segmentation###
+        wsiRenderer = fast.ImagePyramidRenderer.create().connect(importer)
+        segmentationRenderer = fast.SegmentationRenderer.create(borderOpacity=0.5, colors={1: fast.Color.Green()}).connect(stitcher)
+        tissueSegmentationRenderer = fast.SegmentationRenderer.create(borderOpacity=0.5, colors={1: fast.Color.Red()}).connect(tissueSegmentation)
+        # fast.SimpleWindow2D.create().connect(wsiRenderer).connect(segmentationRenderer).run()
+        fast.SimpleWindow2D.create().connect(wsiRenderer).connect(segmentationRenderer).connect(tissueSegmentationRenderer).run()
+    else:
+        exporter.run()
+        print(f'Inference finished. Saved at {os.path.join(output_dir, segmentationFileName)}')
+
 if __name__ == "__main__":
 
-    args = parser.parse_args()
+    # ----- parse command line arguments
+    parser = argparse.ArgumentParser(description='Script to convert QuickAnnotator\'s UNet from PyTorch format to ONNX format')
 
-    epithelium_segmentation_wsi(args.wsi_path, args.model, args.output_folder)
+    parser.add_argument('-m', '--modelPath', help="Path to ONNX model", default='', type=str)
+    parser.add_argument('-w', '--wsiPath', help="Path to input WSI", default='', type=str)
+    parser.add_argument('-p', '--patchSize', help="Size of input in pixels", default=0, type=int)
+
+    args = parser.parse_args()
+    print(f"args: {args}")
+
+    try:
+        assert(args.modelPath != '')
+        assert(args.wsiPath != '')
+        assert(args.patchSize > 0)
+
+        start = time.time()
+
+        fibrosis_segmentation_wsi(args.wsiPath, args.modelPath, visualization=True)
+
+        end = time.time()
+        print(f"inference plus rendering took {end-start} seconds")
+    except Exception as e:
+        print(e)
+        print("Error")
